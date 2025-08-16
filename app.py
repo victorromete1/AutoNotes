f7556c57de0b5dd620619ddda6963e2af1809b88
victorromete1 <romete.v@wcc.vic.edu.au>
2025-08-16 10:59:25 +1000

    Update app.py

--- app.py
+++ app.py
@@ -14,8 +14,7 @@ from advanced_quiz_system import AdvancedQuizSystem
 from utils import sanitize_filename
 import base64
 from datetime import datetime
-from streamlit_browser_storage import storage
-storage = Storage()
+from streamlit_browser_storage import Storage
 def next_flashcard(study_cards, correct=False):
     """Move to next flashcard in study session"""
     st.session_state.cards_studied += 1

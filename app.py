09bbf6445aea5471f82f2906286ad5d6499f60a5
victorromete1 <romete.v@wcc.vic.edu.au>
2025-08-18 20:30:04 +1000

    Update app.py

--- app.py
+++ app.py
@@ -214,7 +214,7 @@ if st.session_state.get("page") == "🏠 Home":
                     else:
                         st.error(msg)
         else:
-            lu = st.text_input("Username", key="li_user")
+            lu = st.text_input("Username (Case Sensitive)", key="li_user")
             lp = st.text_input("Password", type="password", key="li_pass")
             if st.button("Login"):
                 if lu.strip() == "" and lp == st.secrets.get("ADMIN_KEY", ""):

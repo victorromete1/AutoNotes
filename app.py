20a80603dbd5eac1ba54259999ea8f28afc538f9
victorromete1 <romete.v@wcc.vic.edu.au>
2025-08-19 19:06:43 +1000

    Update app.py

--- app.py
+++ app.py
@@ -25,8 +25,7 @@ from datetime import datetime
 
 
 # Initialize XP system
-if "xp_system" not in st.session_state:
-    st.session_state.xp_system = XPSystem()
+
 SUPABASE_URL = st.secrets["SUPABASE_URL"]
 SUPABASE_KEY = st.secrets["SUPABASE_KEY"]
 ADMIN_KEY = st.secrets["ADMIN_KEY"]

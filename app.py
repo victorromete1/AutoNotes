5abbfbcb9e75105d2dece081bf1d2c2b6753f035
victorromete1 <romete.v@wcc.vic.edu.au>
2025-08-21 20:13:31 +1000

    Update app.py

--- app.py
+++ app.py
@@ -46,6 +46,7 @@ ADMIN_KEY = st.secrets["ADMIN_KEY"]  # Used for hidden admin mode toggle
 
 # Supabase Client
 supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
+transcript_data = YouTubeTranscriptApi.get_transcript(video_id)
 
 
 
